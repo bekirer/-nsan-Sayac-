0d06cadb185c4fdd72d8a6a430fb12272f9e419a
bekirer <bekirer.2018@gmail.com>
2023-11-25 14:08:01 +0300

    aa

---
+++ insan sayacı/Person.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b90ecfb6ce5c4f59b804bddc62a4a8afa3d20303db7cee9fdc2b05d5a95b4ce6
+size 2290
---
+++ insan sayacı/counter.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe151114109ef9eff7d578ee251e1f8dd772093c5a83bb51533d12a016718f66
+size 5722
